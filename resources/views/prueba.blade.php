335467939ddb8569ee02c589b83e524b01ce2550
Kevinrente <renteria.krt@gmail.com>
2024-03-10 00:22:18 -0500

    s subir

---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>mira tu correo que te mande un invitacion al repositorio</p>
+    <p>pon tu correo de git y luego tu usuario</p> Kevinrente
+</body>
+</html>
